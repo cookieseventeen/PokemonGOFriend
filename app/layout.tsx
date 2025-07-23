8fdb9a4a53b26b1962bfee91e8b3fe6e9dee2e5a
cookieseventeen <cookie_seventeen@msn.com>
2025-07-24 00:11:22 +0800

    修正 GitHub Pages 部署的路徑問題，將所有絕對路徑改為相對路徑，確保網站圖示和 PWA 功能正常顯示

--- app/layout.tsx
+++ app/layout.tsx
@@ -94,11 +94,11 @@ export default function RootLayout({
         <meta name="rating" content="general" />
         
         {/* 網站圖示 */}
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="icon" href="./favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
+        <link rel="manifest" href="./site.webmanifest" />
         
         {/* 預載入重要資源 */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
